2590675d678cf2b34a7b4f4cf5b972207481a289
VladimirSoya <admin@admin.ru>
2020-09-03 03:25:00 +0700

    fix

---
+++ local/asset/index.php
@@ -0,0 +1,512 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet prefetch"
+				href="https://cdn.rawgit.com/mfd/e7842774e037edf15919037594a79b2b/raw/665bdfc532094318449f1010323c84013d5af953/graphik.css">
+	<title>СД-АВТО</title>
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="node_modules/jquery-modal/jquery.modal.css">
+	<link rel="stylesheet" href="node_modules/ion-rangeslider/css/ion.rangeSlider.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<script
+		src="https://code.jquery.com/jquery-3.5.1.min.js"
+		integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+		crossorigin="anonymous"></script>
+	<script src="js/jquery.anchorScroll.min.js"></script>
+	<script src="node_modules/jquery-modal/jquery.modal.js"></script>
+	<script src="node_modules/ion-rangeslider/js/ion.rangeSlider.js"></script>
+	<script src="node_modules/inputmask/dist/jquery.inputmask.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js"
+					integrity="sha384-qlmct0AOBiA2VPZkMY3+2WqkHtIQ9lSdAsAn5RUJD/3vA5MKDgSGcdmIv4ycVxyn"
+					crossorigin="anonymous"></script>
+	<script src="js/scripts.js"></script>
+</head>
+
+<body>
+<div class="site-container">
+	<header class="header">
+		<div class="container header_container flex">
+
+			<div class="header_logo">
+				<a href="/" class="header_logo-link">
+					<img src="img/svg/sd_icon.svg" alt="ICON" class="header_logo_pic">
+				</a>
+			</div>
+			<div class="icon">
+
+
+				<nav class="icon_city">
+					<img src="img/svg/city1.svg" class="icon_city1"><a href="#">Ваш город:</a>
+					<p class="nsk">Новосибирск </p>
+				</nav>
+				<nav class="icon_geo">
+					<img src="img/svg/geo1.svg" class="icon_geo1">
+					<a href="#contacts" class="anchor-scroll">ул. Кирова 113/3 офис 311</a>
+				</nav>
+				<nav class="icon_call">
+					<img src="img/svg/call.svg" class="icon_call1"><a href="tel:83832479777">8(383)247-9-777</a>
+				</nav>
+				<nav class="icon_number">
+					<!--					<a href="#modal-form" rel="modal:open" class="button">Заказать звонок</a>-->
+					<a href="#one_click" class="button anchor-scroll">Заказать звонок</a>
+				</nav>
+
+			</div>
+
+
+		</div>
+	</header>
+	<main class="main">
+		<div class="background">
+			<!-- First -->
+			<section class="first">
+				<div class="container">
+					<div class="first_girl">
+						<img class="girl_pic" src="img/svg/girl.svg">
+
+						<a href="#calculator" class="button_calc anchor-scroll">
+							<div>
+								<img src="/img/svg/calculator.svg" class="button_calcend">
+							</div>
+							<div class="button_calcend">
+								<div class="calc_text">Калькулятор автокредита</div>
+								<div class="calc_text2">Рассчитайте свой ежемесячный платеж по автокредиту online</div>
+							</div>
+							<div>
+								<img src="/img/svg/play.svg" class="button_calcplay">
+							</div>
+						</a>
+
+					</div>
+					<h1 class="first_title">Автокредит - это просто!
+					</h1>
+					<h2 class="first_subtitle">
+						в Новосибирске
+					</h2>
+
+
+					<ul class="list">
+						<li class="list_el">Одобрим за 15 минут!</li>
+						<li class="list_el">На любое авто Б/у или Новое</li>
+						<li class="list_el">9.9% минимальный годовой процент!</li>
+
+					</ul>
+
+					<!-- bo2tton -->
+					<div class="botton_green_first">
+						<a href="#one_click" class="anchor-scroll button_first">Отправить заявку в 1 клик<img
+								src="/img/svg/send.svg"
+								class="button_send"></a>
+
+						<div class="credit">
+							<h4>Отправка заявки не обязывает
+								Вас брать кредит</h4>
+						</div>
+					</div>
+					<div class="container_info">
+						<div class="info">
+							<div class="mini">
+
+								<img src="/img/svg/mini.svg" class="icon_mini">
+								<div>
+									Минимальный
+									годовой процент
+								</div>
+							</div>
+							<div class="pasport">
+
+								<img src="/img/svg/Pasport.svg" class="icon_pasport">
+								<div>
+									Без справок о доходе
+									Только права и паспорт
+								</div>
+							</div>
+
+							<div class="history">
+
+								<img src="/img/svg/history.svg" class="icon_history">
+								<div>
+									Одобрим с плохой
+									кредитной историей
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</section>
+
+			<!-- Second -->
+			<section class="second">
+				<div class="container">
+					<div class="line_text">
+						<h1 class="second_sd ">СД-АВТО</h1>
+						<p class="second_sd_text second_sd_text_marg ">новый цивилизованный способ безопасной покупки подержанного
+							или нового автомобиля.
+						</p>
+					</div>
+					<div class="second_cards">
+						<div class="second_card1">
+
+							<p class="second_card1_text"> Каждый автомобиль проходит полную техническую диагностику и имеет
+								диагностическую карту.</p>
+							<img src="/img/svg/card1.svg" alt="" style="float: right;
+									margin-top: -18px">
+
+						</div>
+
+						<div class="second_card2">
+
+
+							<p class="second_card2_text"> 100% гарантия юридической чистоты и технической исправности каждого
+								автомобиля.
+							</p>
+							<img src="/img/svg/card2.svg " alt="" style="float: right;
+								margin-top: -26px">
+
+						</div>
+
+						<div class="second_card2">
+
+							<p class="second_card3_text">Предоставляем Автокредит через автосалон на любое новое или подержанное
+								авто. Вы можете выбрать любую машину от любого продавца, не обязательно из салона.
+
+							</p>
+							<img src="/img/svg/card3.svg " alt="" style="float: right;
+								margin-top: -45px">
+
+
+						</div>
+
+						<div class="second_card4">
+
+							<p class="second_card4_text">Автокредит по госпрограмме в 2019 году. Госпрограмма автокредит на первый
+								автомобиль. Программа семейный автокредит в 2019 году
+
+							</p>
+							<img src="/img/svg/card_4.svg" alt="" style="float: right;
+								margin-top: -62px">
+						</div>
+
+
+						<div class="second_card5">
+							<div class="second_card5_pic">
+								<p class="second_card5_title">минимальный годовой процент</p>
+
+								<p class="second_card5_text">9.9% </p>
+								<img src="/img/svg/card5.svg" alt="" style="float: right;
+								margin-top: -78px">
+							</div>
+
+						</div>
+					</div>
+
+
+					<div class="car">
+						<img src="/img/car.png" class="car_pic">
+					</div>
+					<div class="line_text">
+						<h1 class="second_sd second_sd1  "> Почему выгодно взять автокредит через автосалон?</h1>
+					</div>
+					<div class="third_cards">
+						<div class="third_card1">
+							<div class="third_card1_pic">
+								<h4 class="third_card1_title">Проще чем кредит в банке</h4>
+								<img src="/img/svg/01.svg" alt="" class="icon_01">
+								<p class="third_card1_text">Вам больше не нужно идти в банк подтверждать свой доход и собирать справки
+									для
+									получения кредита на авто, достаточно оставить заявку online и прийти в автосалон для получения
+									авто.
+								</p>
+
+							</div>
+
+						</div>
+
+
+						<div class="third_card2">
+							<div class="third_card1_pic">
+								<h4 class="third_card1_title">Экономия времени</h4>
+								<img src="/img/svg/02.svg" alt="" class="icon_02">
+								<p class="third_card1_text">Оставьте заявку на сайте и получите одобрение уже через 15 минут, а
+									забрать
+									авто вы сможете в любое удобное для вас время, без очереди.
+								</p>
+
+							</div>
+
+						</div>
+
+						<div class="third_card2">
+							<div class="third_card1_pic">
+
+								<h4 class="third_card1_title">Помощь в подборе авто</h4>
+								<img src="/img/svg/03.svg" alt="" class="icon_03">
+								<p class="third_card1_text">Предлагаем бесплатно воспользоваться помощью наших специалистов по
+									автоподбору
+								</p>
+								<a href="#one_click" class="third_card1_more anchor-scroll"> Подробнее об автоподборе <img
+										src="/img/svg/Arrow.svg"
+										class="arrow">
+								</a>
+
+
+							</div>
+
+
+						</div>
+					</div>
+
+					<div id="calculator" class="line_text">
+						<h1 class="second_sd 1"> Калькулятор автокредита
+						</h1>
+						<p class="second_sd_text second_sd_text_marg">Рассчитайте свой ежемесячный платеж по автокредиту online
+						</p>
+					</div>
+				</div>
+
+			</section>
+			<!--Calculator  -->
+			<section class="calculator second container">
+
+				<div class="calc_main">
+					<div class="calc_over_l calc_over_margin">
+						Выберите сумму автокредита, руб.
+						<span class="calc_over_r color-gray">
+								<span class="sum_credit">1 230 380</span> <span class="calc_over_rub">руб</span>
+							</span>
+					</div>
+					<div class="line_pic">
+						<input type="text" class="js-range-slider-1" value=""/>
+						<!--						<img src="/img/svg/line_gray1.svg" class="line_gray">-->
+						<!--						<img src="/img/svg/line_green.svg" class="line_green">-->
+						<!--						<img src="/img/svg/circle_line.svg" class="circle_line">-->
+					</div>
+					<div class="calc_under_l">
+						0
+						<span class="calc_under_r">
+								2 млн
+							</span>
+					</div>
+
+				</div>
+				<div class="calc_main">
+					<div class="calc_over_l calc_over_margin">
+						Срок кредитования, лет.
+						<span class="calc_over_r color-gray">
+							<span class="term_credit">8</span> <span class="calc_over_text">лет</span>
+							</span>
+					</div>
+					<div class="line_pic">
+						<!--						<img src="/img/svg/line_gray1.svg" class="line_gray">-->
+						<!--						<img src="/img/svg/line_green.svg" class="line_green">-->
+						<!--						<img src="/img/svg/circle_line.svg" class="circle_line">-->
+						<input type="text" class="js-range-slider-2" value=""/>
+					</div>
+					<div class="calc_under_l">
+						0
+						<span class="calc_under_r">
+								15
+							</span>
+					</div>
+
+				</div>
+
+
+				<div class="calc_end">
+					<div class="calc_over_l calc_over_end">
+						Ваш ежемесячный платеж, руб:
+						<span class="calc_over_r color-green">
+								<span class="calc_result">30 380</span> <span class="calc_over_rub">руб</span>
+							</span>
+					</div>
+					<div class="calc_under_end">
+						*Рассчет ежемесячного платежа выполнен по минимальной годовой ставке 9,9%
+					</div>
+				</div>
+			</section>
+			<!-- Button -->
+			<div class=" container ">
+				<div class="botton_green">
+					<a href="#one_click" class="anchor-scroll button_first">Отправить заявку в 1 клик<img
+							src="/img/svg/send.svg"
+							class="button_send"></a>
+
+					<div class="credit">
+						<h4>Отправка заявки не обязывает
+							Вас брать кредит</h4>
+					</div>
+				</div>
+				<div class="line_text ">
+					<h1 class="second_sd 1"> Как получить Автокредит ?
+					</h1>
+					<p class="second_sd_text credit_text  second_text_margin">На любой подержанный Б/у или Новый автомобиль в
+						Новосибирске, не обязательно из автосалона!</p>
+				</div>
+			</div>
+			<!-- Question 5 colomn -->
+
+			<div class="question container">
+				<div class="question_card1">
+					<img src="/img/svg/request.svg" alt="">
+					<p class="question_card1_text">Оставьте
+						заявку
+					</p>
+
+				</div>
+				<div class="question_card2">
+					<img src="/img/svg/30sec.svg" alt="">
+					<p class="question_card2_text">Мы перезвоним
+						за 30 секунд
+					</p>
+
+				</div>
+				<div class="question_card3">
+					<img src="/img/svg/chat.svg" alt="">
+					<p class="question_card3_text">Проконсультируем и ответим на все вопросы по Автокредитованию
+					</p>
+
+				</div>
+				<div class="question_card4">
+					<img src="/img/svg/money.svg" alt="">
+					<p class="question_card4_text">Одобрим нужную вам сумму
+					</p>
+
+				</div>
+				<div class="question_card5">
+					<img src="/img/svg/smile.svg" alt="">
+					<p class="question_card5_text">Заберите авто
+						в удобное время
+					</p>
+
+				</div>
+			</div>
+		</div>
+		<!-- Request -->
+		<section class="request container">
+			<h3 class="request_text">Оставьте заявку, чтобы получить бесплатную консультацию</h3>
+			<img id="one_click" src="/img/svg/boy.svg" class="boy_pic" style="float: right ;
+margin: -195px -6px 0px 0px;">
+			<!-- Form -->
+			<form action="form.php" class="search_form">
+				<div class="search_form_info">
+					<input type="text" name="name" class="search_form_fild" placeholder="Имя" required>
+					<input type="text" id="phone" name="phone" class="search_form_fild" placeholder="Номер телефона" required>
+					<div class="button_main">
+						<button type="submit" class="search_form_submit">Отправить заявку<img src="/img/svg/send.svg"
+																																									class="button_send"></button>
+
+						<h4 class="search_form_text">*Я даю согласие на обработку персональных данных
+						</h4>
+					</div>
+
+				</div>
+			</form>
+		</section>
+		<!-- Contacts -->
+		<section id="contacts" class="contacts">
+			<div class="container">
+				<h3 class="contacts_h3">Остались вопросы?</h3>
+				<h5 class="contacts_h5">Свяжитесь с нами!</h5>
+
+				<!--contacts_info  -->
+				<div class="contacts_info">
+
+					<div>
+						<div class="contacts__address">
+							<div class="contacts__address_pic">
+								<img src="/img/svg/geo_icon.svg" alt="" class="contacts__img">
+								<h4 class="contacts__title">Адрес</h4>
+								<p class="contacts_text">г. Новосибирск, Кирова 113/3 офис 311
+								</p>
+
+							</div>
+
+						</div>
+
+						<div class="contacts__time">
+							<div class="contacts__address_pic">
+								<img src="/img/svg/time_icon.svg" alt="" class="contacts__img">
+								<h4 class="contacts__title">Режим работы</h4>
+
+								<p class="contacts_text">Ежедневно с 9.00 до 19.00
+								</p>
+
+							</div>
+
+						</div>
+
+						<div class="contacts__call">
+							<div class="contacts__address_pic">
+								<img src="/img/svg/call_icon.svg" alt="" class="contacts__img">
+								<h4 class="contacts__title">Телефон</h4>
+
+								<p class="contacts_text"><a href="tel:84959000000">(495) 900-00-00</a>
+								</p>
+
+							</div>
+
+						</div>
+
+						<div class="contacts__letter">
+							<div class="contacts__address_pic">
+								<img src="/img/svg/letter_icon.svg" alt="" class="contacts__img">
+								<h4 class="contacts__title">E-mail</h4>
+
+								<p class="contacts_text"><a href="mailto:info@sdauto.ru?subject=Автокредит">info@sdauto.ru</a>
+								</p>
+
+							</div>
+
+						</div>
+					</div>
+					<div class="map_desktop">
+						<script type="text/javascript" charset="utf-8" async
+										src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Af24f861433f938f2c637e0f94e7eafe282263616617b0faa626556018d831e03&amp;width=500&amp;height=340&amp;lang=ru_RU&amp;scroll=true"></script>
+					</div>
+				</div>
+
+			</div>
+
+		</section>
+		<section class="request map_mobile">
+			<script type="text/javascript" charset="utf-8" async
+							src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Af24f861433f938f2c637e0f94e7eafe282263616617b0faa626556018d831e03&amp;width=100%25&amp;height=340&amp;lang=ru_RU&amp;scroll=true"></script>
+		</section>
+		<section class="footer">
+			<div class="container">
+				<img src="/img/svg/logo_end.svg" class="logo_end">
+				<h4 class="footer_ooo">ОГРН 1195476042651 ИНН 5405042720
+					© ООО «СД-АВТО» 2020 г. </h4>
+				<p class="footer_text">Обращаем ваше внимание на то, что данный интернет-сайт носит исключительно информационный
+					характер и ни при каких условиях не является публичной офертой, определяемой положениями ч. 2 ст. 437
+					Гражданского кодекса Российской Федерации. Все персональные данные не подлежат передачи третьим лицам и
+					защищены федеральным законом Российской Федерации от 27 июля 2006 г. № 152-ФЗ.</p>
+				<p class="footer_text">
+					ООО "АвтоМолл" ОГРН 1195476042651 Банк-партнер: "Тинькофф Банк" (АО), Лицензия на осуществление банковских
+					операций № 2673 от 24.03.2015 , Учётный номер в Росфинмониторинге: 704012178</p>
+				<p class="footer_text">
+					Copyrights © 2017 All Rights Reserved Inc.</p>
+				<a href="#" class="footer_doc">Агентский договор</a>
+				<a href="#" class="footer_doc footer_marg">Лицензия</a>
+			</div>
+
+		</section>
+	</main>
+
+</div>
+</body>
+<div style="display: none;" id="modal-form">
+	<h2>Hello</h2>
+	<p>You are awesome.</p>
+</div>
+<div style="display: none;" id="modal-ok">
+	<h2 style="text-align: center;
+    font-size: 18px; padding: 10px 0 0 0;">Спасибо!</h2>
+	<p style="text-align: center;
+    font-size: 16px; padding: 15px;">Заявка отправлена</p>
+</div>
+</html>
---
+++ local/asset/js/scripts.js
@@ -0,0 +1,108 @@
+'use strict';
+var anchorScroll = (function () {
+	var init = function () {
+		jQuery('.anchor-scroll').anchorScroll({
+			scrollSpeed: 1000, // scroll speed
+			offsetTop: 0, // offset for fixed top bars (defaults to 0)
+			onScroll: function () {
+				// callback on scroll start
+			},
+			scrollEnd: function () {
+				// callback on scroll end
+			}
+		});
+	};
+	return {
+		init: init,
+	};
+})();
+
+var calculator = (function () {
+	var init = function () {
+		var $range1 = $(".js-range-slider-1");
+		var $range2 = $(".js-range-slider-2");
+		var v1 = 0;
+		var v2 = 0;
+
+		$range1.ionRangeSlider({
+			skin: "round",
+			type: "single",
+			min: 0,
+			max: 2000000,
+			from: 1230380,
+			hide_min_max: true,
+			grid: false,
+			onFinish: function (data) {
+				v1 = data.from_pretty;
+				jQuery('.sum_credit').html(v1);
+				calc();
+			}
+		});
+
+		$range2.ionRangeSlider({
+			skin: "round",
+			type: "single",
+			min: 0,
+			max: 15,
+			from: 8,
+			hide_min_max: true,
+			onFinish: function (data) {
+				v2 = data.from_pretty;
+				jQuery('.term_credit').html(v2);
+				calc();
+			}
+		});
+
+		function calc() {
+			let p = 0,
+				s = $range1.prop("value"),
+				r = 0.01,
+				n = $range2.prop("value") * 12;
+
+			p = s * (r * (1 + r) ** n) / ((1 + r) ** n - 1)
+			jQuery('.calc_result').html(p.toFixed());
+		}
+	};
+	return {
+		init: init,
+	};
+})();
+
+var formBottom = (function () {
+	var sendForm = function () {
+
+		var options = {
+			success: showResponse,
+			type: 'post',
+			resetForm: true
+		};
+
+		jQuery('.search_form').ajaxForm(options);
+
+		function showResponse(responseText, statusText, xhr, $form) {
+			$('#modal-ok').modal();
+		}
+
+	};
+	var mask = function () {
+
+		var selector = document.getElementById("phone");
+		var selector_modal = document.getElementById("phone-modal");
+
+		var im = new Inputmask("+7 999-999-99-99");
+		im.mask(selector);
+		im.mask(selector_modal);
+
+	};
+	return {
+		sendForm: sendForm,
+		mask: mask,
+	};
+})();
+
+$(function () {
+	anchorScroll.init();
+	calculator.init();
+	formBottom.sendForm();
+	formBottom.mask();
+});

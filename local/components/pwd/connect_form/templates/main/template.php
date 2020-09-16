70ce40a46e7023700a0b60e4288df8687051d78c
VladimirSoya <admin@admin.ru>
2020-09-17 04:33:08 +0700

    fix

--- local/components/pwd/connect_form/templates/main/template.php
+++ local/components/pwd/connect_form/templates/main/template.php
@@ -1,6 +1,6 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
-<h3 class="request_text">Оставьте заявку, чтобы получить бесплатную консультацию</h3>
-<img id="one_click" src="/local/asset/img/svg/boy.svg" class="boy_pic" style="float: right ;
+<h3 id="one_click" class="request_text">Оставьте заявку, чтобы получить бесплатную консультацию</h3>
+<img src="/local/asset/img/svg/boy.svg" class="boy_pic" style="float: right ;
 margin: -195px -6px 0px 0px;">
 <!-- Form -->
 <form id="main-form-send" class="search_form">
@@ -20,5 +20,5 @@ margin: -195px -6px 0px 0px;">
             </div>
         </div>
     </div>
-        <img id="one_click" src="/local/asset/img/svg/boy.svg" class="boy_pic mobile_boy_pic">
+        <img src="/local/asset/img/svg/boy.svg" class="boy_pic mobile_boy_pic">
 </form>
